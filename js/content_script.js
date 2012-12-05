e3cc799bef1d7e430005f8d6de2c22f57e94bc08
shendongming <shendongming2010@gmail.com>
2012-12-05 10:32:19 +0800

    增加 google 资讯url 匹配

--- js/content_script.js
+++ js/content_script.js
@@ -23,7 +23,16 @@ if(location.toString().indexOf('baidu.com')>-1){
     }
 }
 if(location.toString().indexOf('google.com')>-1){
-    var links=document.querySelectorAll('h3.r a');
+    var links3=document.querySelectorAll('h3.r a');
+    var links2=document.querySelectorAll('span.tl a');
+    var links=[]
+    for(var i =0;i<links2.length;i++){
+        links.push(links2[i])
+    }
+    for(var i =0;i<links3.length;i++){
+        links.push(links3[i])
+    }
+
     for(var i =0;i<links.length;i++){
         var a=links[i];
         var info=util.parse_url(a.href,location.toString());
@@ -33,7 +42,8 @@ if(location.toString().indexOf('google.com')>-1){
         a2.setAttribute('title',a.href);
         a2.target='_blank';
         a2.innerHTML=a.href;
-        a2.style.cssText='margin:5px;color:blue;padding-left:20px;font-weight:bold;clear:both;display:block'
+        a2.style.cssText='margin:5px;color:blue;padding-left:20px;font-weight:bold;clear:both;display:block';
+       
         a.parentNode.appendChild(a2)
         console.log(info);
     }
